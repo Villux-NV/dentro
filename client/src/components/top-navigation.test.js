d5a5faf8c1ffeac7a0e145184695caca20181318
Nick Villarreal <nick33v@gmail.com>
2021-03-18 09:11:39 -0400

    push merge

--- client/src/components/top-navigation.test.js
+++ client/src/components/top-navigation.test.js
@@ -1,27 +1,37 @@
 import React from 'react';
-import { render, getQueriesForElement } from '@testing-library/react';
+import { render, getQueriesForElement, getByRole } from '@testing-library/react';
 import { config } from 'dotenv';
 import path from 'path';
-// import Firebase from '../firebase';
 
 import { AuthProvider, AuthContext } from './auth';
-import App from '../App';
 import TopNavigation from './top-navigation';
 
 config({
   path: path.resolve(process.cwd(), '../../.env.test.local')
 })
 
-const currentUser = null;
+const currentUser = {
+  displayName: 'Test User'
+};
 
-it('renders top navigation', () => {
+test('renders top navigation', () => {
   // console.log(Firebase);
-  const { getByText, getByLabelText } = render(
+  const { getByText } = render(
     <AuthContext.Provider value={{currentUser}}>
       <TopNavigation />
     </AuthContext.Provider>
   );
 
   getByText('Dentro');
-  // expect(root.querySelector('a').textContent).toBe('Dentro');
+});
+
+test('displays user name', () => {
+  const { getByText, getByLabelText } = render(
+    <AuthContext.Provider value={{currentUser}}>
+      <TopNavigation />
+    </AuthContext.Provider>
+  );
+
+  
+  expect(getByText(/Logout/)).toHaveTextContent('Test User');
 });
